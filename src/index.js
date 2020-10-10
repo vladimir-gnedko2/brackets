5dd5b62c393e2745a00b30b32350ee3f0607063e
vladimir-gnedko2 <vavanchikmks@gmail.com>
2020-10-10 20:56:15 +0300

    add: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var stack = [];
+  var matches = {};
+
+  bracketsConfig.forEach((x) => (matches[x[0]] = x[1]));
+
+  for (var i = 0; i < str.length; i++) {
+      var bracket = str.charAt(i);
+      if (matches[bracket] == bracket && stack[stack.length - 1] == bracket) {
+          stack.pop();
+      } else if (matches[bracket]) {
+          stack.push(bracket);
+      } else if (bracket != matches[stack.pop()]) {
+          return false;
+      }
+  }
+  return stack.length == 0;
+};
